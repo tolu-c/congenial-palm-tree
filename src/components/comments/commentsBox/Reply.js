d718117d9ee01f306ac9fe0829feaf12c20fcb24
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-13 23:17:16 +0100

    fixup

--- src/components/comments/commentsBox/Reply.js
+++ src/components/comments/commentsBox/Reply.js
@@ -2,9 +2,9 @@ import { ReplyIcon } from "@heroicons/react/solid";
 
 export default function Reply() {
   return (
-    <div className="flex items-center gap-1 text-blue-900 hover:text-blue-200/50 cursor-pointer justify-items-end md:items-start">
-      <ReplyIcon className="h-3 w-3 stroke-[4px]" />
-      <span className="text-xs font-bold">Reply</span>
+    <div className="flex items-center gap-1 text-blue-900 hover:text-blue-200/80 cursor-pointer justify-items-end md:items-start">
+      <ReplyIcon className="h-3 w-3 md:w-4 md:h-4 stroke-[4px]" />
+      <span className="text-xs md:text-sm font-bold">Reply</span>
     </div>
   );
 }
--- src/pages/comments/Comments.js
+++ src/pages/comments/Comments.js
@@ -8,7 +8,7 @@ function Comments() {
   return (
     <main className="bg-gradient-to-br from-blue-200/75 via-purple-400 to-blue-600 flex items-center justify-center h-screen">
       <BackButton />
-      <div className="w-5/6 md:max-w-xl mx-auto p-2 md:pt-4 grow flex flex-col gap-2 bg-white rounded-lg md:relative h-auto md:h-40">
+      <div className="w-5/6 md:max-w-xl mx-auto p-2 md:pt-4 flex flex-col gap-2 bg-white rounded-lg md:relative h-auto md:h-40">
         <div className="flex w-full justify-between order-last md:order-first">
           <Vote />
           <Reply />
