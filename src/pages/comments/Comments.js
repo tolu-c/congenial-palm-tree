bf0b60e945f3293048640243391ef9a750a7de84
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-13 23:24:55 +0100

    multiple comments

--- src/pages/comments/Comments.js
+++ src/pages/comments/Comments.js
@@ -6,20 +6,29 @@ import BackButton from "../../components/ui/BackButton";
 
 function Comments() {
   return (
-    <main className="bg-gradient-to-br from-blue-200/75 via-purple-400 to-blue-600 flex items-center justify-center h-screen">
+    <main className="bg-gradient-to-br from-blue-200/75 via-purple-400 to-blue-600 flex flex-col gap-4 overflow-scroll items-center justify-center h-screen">
       <BackButton />
-      <div className="w-5/6 md:max-w-xl mx-auto p-2 md:pt-4 flex flex-col gap-2 bg-white rounded-lg md:relative h-auto md:h-40">
-        <div className="flex w-full justify-between order-last md:order-first">
-          <Vote />
-          <Reply />
-        </div>
-        <div className="flex flex-col gap-2 md:w-5/6 md:absolute md:left-12 md:h-full">
-          <Info />
-          <Body />
-        </div>
-      </div>
+      <SingleComment />
+      <SingleComment />
+      <SingleComment />
+      <SingleComment />
     </main>
   );
 }
 
 export default Comments;
+
+function SingleComment() {
+  return (
+    <div className="w-5/6 md:max-w-xl mx-auto p-2 md:pt-4 flex flex-col gap-2 bg-white rounded-lg md:relative h-auto md:h-40 shadow-md">
+      <div className="flex w-full justify-between order-last md:order-first">
+        <Vote />
+        <Reply />
+      </div>
+      <div className="flex flex-col gap-2 md:w-5/6 md:absolute md:left-12 md:h-full">
+        <Info />
+        <Body />
+      </div>
+    </div>
+  );
+}
