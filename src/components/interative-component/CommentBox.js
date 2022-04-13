e4e71c6bb34c851451f1e79108b18b34497499fe
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-13 19:43:26 +0100

    added breakpoint

--- src/components/interative-component/CommentBox.js
+++ src/components/interative-component/CommentBox.js
@@ -3,7 +3,7 @@ import CommentBoxMain from "./CommentBoxMain";
 
 function CommentBox() {
   return (
-    <div className='w-5/6 max-w-xl mx-auto p-2 flex gap-4 bg-white rounded-lg'>
+    <div className='w-5/6 md:max-w-xl mx-auto p-2 flex gap-4 bg-white rounded-lg'>
       <CommentBoxSide />
       <CommentBoxMain />
     </div>
